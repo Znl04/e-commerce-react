69967b51c70476fc0a60996bbd61f9e7ef7404f7
Nurzhan Zeinolla <zeinolla.nur04@gmail.com>
2025-01-17 01:08:09 +0500

    feat: finished category component

--- src/Sidebar/Category/Category.js
+++ src/Sidebar/Category/Category.js
@@ -2,6 +2,30 @@ import './Category.css';
 
 export default function Category() {
   return(
-    <div>Category</div>
+    <div>
+      <h2 className="sidebar-title">Category</h2>
+      <div className="">
+        <label className="sidebar-label-container">
+          <input type="radio" name='test'/>
+          <span className="chekmark"></span>All
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name='test'/>
+          <span className="chekmark"></span>Sneakers
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name='test'/>
+          <span className="chekmark"></span>Flats
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name='test'/>
+          <span className="chekmark"></span>Sandals
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name='test'/>
+          <span className="chekmark"></span>Heels
+        </label>
+      </div>
+    </div>
   );
 }
