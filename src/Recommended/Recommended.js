38bc03a672888a79430c86184747356973f5c815
Nurzhan Zeinolla <zeinolla.nur04@gmail.com>
2025-01-17 00:22:15 +0500

    feat: finished recommended section

--- src/Recommended/Recommended.js
+++ src/Recommended/Recommended.js
@@ -2,6 +2,14 @@ import './Recommended.css';
 
 export default function Recommended() {
   return(
-    <div>Recommended</div>
+    <>
+      <h2 className='recommended-title'>Recommended</h2>
+      <div className="recommended-btns">
+        <button className='btns'>All products</button>
+        <button className='btns'>Nike</button>
+        <button className='btns'>Puma</button>
+        <button className='btns'>Vans</button>
+      </div>
+    </>
   );
 }
